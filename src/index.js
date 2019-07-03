59e9699ec2aeedeae8fb0656c4cfa2de2ad6291a
Cormac Levins <cormaclevins@outlook.ie>
2019-07-03 16:15:51 +0100

    JWT Auth added to endpoints

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-app.use(taskRouter) 
+app.use(taskRouter)
 
 app.listen(port, () => {
   console.log('Server open on: ' + port)
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,8 +40,23 @@ const userSchema = new mongoose.Schema({
         throw new Error("Password can't contain 'password' ");
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  const token = jwt.sign({_id: user._id.toString()}, 'topgear')
+
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+  return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })

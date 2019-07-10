b6c4c939b1689c4a6afe1e391acedb042008e2b0
Cormac Levins <cormaclevins@outlook.ie>
2019-07-10 13:34:11 +0100

    Logout All Endpoint Added

--- src/models/user.js
+++ src/models/user.js
@@ -58,6 +58,15 @@ userSchema.methods.generateAuthToken = async function() {
   return token
 }
 
+userSchema.methods.toJSON = function () {
+  const user = this
+
+  const userObj = user.toObject()
+  delete userObj.password
+  delete userObj.tokens
+  return userObj
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 

e374cde5a532c094041bbbc64144ec7933f6b81a
Cormac Levins <cormaclevins@outlook.ie>
2019-09-16 17:21:55 +0100

    Image Upload Added

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
+
 
 router.post('/users', async (req, res) => {
 
@@ -21,6 +24,42 @@ router.get('/users/me', auth ,async (req, res) => {
 
   res.send(req.user)
 
+})
+
+
+  const upload = multer( {
+    limits: {
+      fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+
+      if(!file.originalname.match(/\.(jpeg|jpg|png)$/)) {
+        return cb(new Error('File Not Supported'))
+      }
+
+      cb(undefined, true)
+    }
+  })
+
+
+router.post('/users/me/avatar', auth, upload.single('avatar') , async (req, res) => {
+
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+
+    //req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+
+router.delete('users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
 })
 
 router.post('/users/login', async (req, res) => {
@@ -46,6 +85,8 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 })
 
+
+
 router.post('/users/logoutAll', auth, async (req, res) => {
   try{
     req.user.tokens = []
@@ -54,6 +95,21 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send()
   }
+})
+
+router.get('users/:id/avatar', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id)
+
+      if (!user || !user.avatar) {
+        throw new Error()
+      }
+
+      res.set('Content-Type', 'image/png')
+      res.send(user.avatar)
+    } catch (e) {
+      res.status(404).send()
+    }
 })
 
 
